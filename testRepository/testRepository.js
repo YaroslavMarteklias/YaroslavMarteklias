733834685d184437ddb0080c82adb05f41a80b04
yaroslavmarteklias <martekliasy@gmail.com>
2023-12-27 16:05:48 +0200

    testRepository

---
+++ testRepository/testRepository.js
@@ -0,0 +1,13 @@
+{
+   "firstName": "Yaroslav",
+   "lastName": "Marteklias",
+   "address": {
+       "streetAddress": "Skoropadsky 6",
+       "city": "Truskavets",
+       "postalCode": 82200
+   },
+   "phoneNumbers": [
+       "0973813165",
+       "0968227813"
+   ]
+}
